335ddfb87002645fa413527b192300ae5d20e891
Barden-dev <barden2410@gmail.com>
2025-06-28 20:02:47 +0300

    add many files

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import time
-from flask import Flask
 import redis
 import redis.exceptions
+import os
+
+from flask import Flask
+from dotenv import load_dotenv
+
 
 app = Flask(__name__)
 
-cache = redis.Redis(host='redis', port=6379)
+load_dotenv()
+redis_host = os.getenv('redis_host', 'redis')
+
+cache = redis.Redis(host=redis_host, port=6379)
 
 def get_hit_count():
     retries = 5
