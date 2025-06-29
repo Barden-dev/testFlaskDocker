48d088231b66fb3294d53156197d8bed3cf640d0
Barden-dev <barden2410@gmail.com>
2025-06-29 22:32:21 +0300

    second test

--- test_app.py
+++ test_app.py
@@ -1,6 +1,13 @@
 from app import app
+from dotenv import load_dotenv
+import os
 
 def test_hello():
+    load_dotenv()
+    redis_host = os.getenv('redis_host', 'redis')
+    
+    print(redis_host)
+    
     with app.test_client() as client:
         response = client.get('/')
         assert response.status_code == 200
