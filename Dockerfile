e5b8d36e3db1726fcbd284b3fcc78f95ca55b65e
Barden-dev <barden2410@gmail.com>
2025-06-29 23:00:46 +0300

    doubletest

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -i https://pypi.org/simple/ -r requirements.txt
 COPY . .
 EXPOSE 5000
-CMD ["python", "-m", "flask", "run", "--host=0.0.0.0"]  
+CMD ["gunicorn", "--bind", "0.0.0.0:5000", "app:app"]
--- test_app.py
+++ test_app.py
@@ -1,13 +1,6 @@
 from app import app
-from dotenv import load_dotenv
-import os
 
 def test_hello():
-    load_dotenv()
-    redis_host = os.getenv('redis_host', 'redis')
-    
-    print(redis_host)
-    
     with app.test_client() as client:
         response = client.get('/')
         assert response.status_code == 200
